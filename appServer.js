8a0e3ffffd4501325a43a2b800e197cc1b284ae8
Gannon Curran <gannoncurran@gmail.com>
2016-06-12 19:34:21 -0700

    refine appServer.js

--- appServer.js
+++ appServer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console, global-require */
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -9,15 +10,15 @@ const isDeveloping = process.env.NODE_ENV !== 'production';
 const app = express();
 
 if (isDeveloping) {
-  console.log('DEVELOPMENT: Serving with WebPack Middleware'); // eslint-disable-line no-console
-  const webpack = require('webpack'); // eslint-disable-line global-require
-  const webpackMiddleware = require('webpack-dev-middleware'); // eslint-disable-line global-require
-  const webpackHotMiddleware = require('webpack-hot-middleware'); // eslint-disable-line global-require, max-len
-  const config = require('./webpack.config.js'); // eslint-disable-line global-require
+  console.log('DEVELOPMENT: Serving with WebPack Middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  const compiler = webpack(webpackConfig);
 
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackDevMiddlewareOptions = {
+    publicPath: webpackConfig.output.publicPath,
     contentBase: 'src',
     stats: {
       colors: true,
@@ -27,16 +28,20 @@ if (isDeveloping) {
       chunkModules: false,
       modules: false,
     },
-  });
-  app.use(middleware);
+  };
+  const devMiddleware = webpackDevMiddleware(compiler, webpackDevMiddlewareOptions);
+
+  app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(devMiddleware);
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.join(__dirname, 'assets')));
   app.get('*', (req, res) => {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'public', 'index.html')));
+    res.write(devMiddleware.fileSystem.readFileSync(path.join(__dirname, 'public', 'index.html')));
     res.end();
   });
 } else {
-  console.log('PRODUCTION: Serving built files from public/index.html'); // eslint-disable-line no-console, max-len
+  console.log('PRODUCTION: Serving built files from assets/ and then public/ respectively');
   app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
@@ -47,36 +52,36 @@ if (isDeveloping) {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+}
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('404: Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  // error handlers
+// error handlers
 
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use((err, req, res) => {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err,
-      });
-    });
-  }
-
-  // production error handler
-  // no stacktraces leaked to user
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
   app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {},
+      error: err,
     });
   });
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
 
 module.exports = app;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
-import React, {
-  PropTypes,
-} from 'react';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 import '../styles/normalize.css';
 import '../styles/app.scss';
@@ -9,7 +8,12 @@ const App = ({
   children,
 }) => (
   <div className="container">
+    <div className="logo" />
     <h1 className="headline">App</h1>
+    <nav className="nav">
+      <Link className="nav__item" to={'/home'}>Home</Link>
+      <Link className="nav__item" to={'/subpage'}>Subpage</Link>
+    </nav>
     {children}
   </div>
 );
